0feef6c02bb5bdbf0a215c16d02984a7fcae321c
Máté Szabó <szmate1618@gmail.com>
2017-06-28 09:31:56 +0200

    add ViewTransformationLog

---
+++ FishEyeDemo/FishEyeDemo/ViewTransformationLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FishEyeDemo
+{
+    class ViewTransformationLog: ViewTransformation
+    {
+        public Point ModelPointToPoint(ModelPoint mp)
+        {
+            ModelPoint euclidean = new ModelPoint(mp.x - offset_x, -offset_y + mp.y);
+            double distance = euclidean.Length();
+            if (distance <= 128.7463)
+            {
+                return new Point(Convert.ToInt32(300 + zoom * euclidean.x),
+                                 Convert.ToInt32(300 - zoom * euclidean.y));
+            }
+            else
+            {
+                return new Point(Convert.ToInt32(300 + zoom * euclidean.x / distance * Math.Log(distance + 1, 1.5) / 12 * 129.7463),
+                                 Convert.ToInt32(300 - zoom * euclidean.y / distance * Math.Log(distance + 1, 1.5) / 12 * 129.7463));
+            }
+        }
+
+    }
+}
