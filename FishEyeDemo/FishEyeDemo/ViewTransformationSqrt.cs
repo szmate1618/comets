6c37e2d4daa971b801e7fb4074402fb44e0221ee
Máté Szabó <szmate1618@gmail.com>
2017-06-28 09:31:42 +0200

    add ViewTransformationSqrt

---
+++ FishEyeDemo/FishEyeDemo/ViewTransformationSqrt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FishEyeDemo
+{
+    class ViewTransformationSqrt: ViewTransformation
+    {
+        public Point ModelPointToPoint(ModelPoint mp)
+        {
+            ModelPoint euclidean = new ModelPoint(mp.x - offset_x, -offset_y + mp.y);
+            double distance = euclidean.Length();
+            if (distance <= 100)
+            {
+                return new Point(Convert.ToInt32(300 + zoom * euclidean.x),
+                                 Convert.ToInt32(300 - zoom * euclidean.y));
+            }
+            else
+            {
+                return new Point(Convert.ToInt32(300 + zoom * euclidean.x / distance * Math.Sqrt(distance) * 10),
+                                 Convert.ToInt32(300 - zoom * euclidean.y / distance * Math.Sqrt(distance) * 10));
+            }
+        }
+    }
+}
