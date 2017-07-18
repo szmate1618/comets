ece1f578e1cb514e7f0854a2da4de1d1db39b124
Máté Szabó <szmate1618@gmail.com>
2017-07-18 10:02:49 +0200

    refactor Iterator test, add new testcase

--- Game/Test/TestSimplePartition.cpp
+++ Game/Test/TestSimplePartition.cpp
@@ -17,24 +17,30 @@ namespace Test
 		typedef entity::StaticEntity* const pointer;
 		typedef entity::Iterator<pointer> iterator;
 
-	public:
-		
-		TEST_METHOD(TestAddNumbers)
+		entity::SimplePartition InitPartition(long input_array[], long count)
 		{
 			entity::SimplePartition partition;
+			assert(count <= partition.getCapacity());
+			for(long i = 0; i < count; ++i)
+			{
+				partition.Add((pointer)input_array[i]);
+			}
+			return partition;
+		}
 
-			const long n = 10000; //TODO: Refactor these into an initializer method.
-			assert(n >= partition.getCapacity());
+	public:
+
+		TEST_METHOD(TestFill)
+		{
+			const long n = 10000;
+			assert(n >= entity::SimplePartition::getCapacity()); //Make sure we generate enough elements to fill the full capacity.
 			long numbers[n];
 			for (long i = 0; i < n; i++)
 			{
 				numbers[i] = i + 1;
 			}
+			entity::SimplePartition partition = InitPartition(numbers, entity::SimplePartition::getCapacity()); //Fill up to full capacity.
 
-			for(long i = 0; i < partition.getCapacity(); ++i)
-			{
-				partition.Add((pointer)numbers[i]);
-			}
 
 			long j = 1;
 			for (iterator i = partition.begin(); i != partition.end(); i++)
@@ -46,7 +52,34 @@ namespace Test
 
 		TEST_METHOD(TestNestedLoops)
 		{
+			long numbers[] = {1, 2};
+			entity::SimplePartition partition = InitPartition(numbers, sizeof(numbers) / sizeof(long));
+			long accumulator = 0;
+			for(iterator i: partition)
+			{
+				for(iterator j: partition)
+				{
+					(accumulator *= 100) += 10 * (long)(*i) + (long)(*j);
+				}
+			}
+			Assert::AreEqual(11122122L, accumulator, L"Apparently things got tangled up in this nested iteration.");
 		}
 
+		TEST_METHOD(TestNestedLoopsWithContinue)
+		{
+			long numbers[] = {1, 2, 3};
+			entity::SimplePartition partition = InitPartition(numbers, sizeof(numbers) / sizeof(long));
+			long accumulator = 0;
+			for(iterator i: partition)
+			{
+				if ((long)(*i) == 1) continue;
+				for(iterator j: partition)
+				{
+					if ((long)(*j) == 2) continue;
+					(accumulator *= 100) += 10 * (long)(*i) + (long)(*j);
+				}
+			}
+			Assert::AreEqual(21233133L, accumulator, L"Apparently things got tangled up in this continued nested iteration.");
+		}
 	};
 }
