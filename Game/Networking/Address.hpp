85783fc7e88ea28d6904b5983a55364fa9f43947
Máté Szabó <szmate1618@gmail.com>
2017-08-12 11:02:51 +0200

    fix spacing

--- Game/Networking/Address.hpp
+++ Game/Networking/Address.hpp
@@ -9,25 +9,20 @@ namespace net
 	public:
 
 		Address() = delete;
-
-		Address(unsigned int,
+		Address
+		(
 			unsigned int,
 			unsigned int,
 			unsigned int,
-			unsigned short);
-		
+			unsigned int,
+			unsigned short
+		);
 		Address(unsigned int, unsigned short);
-		
 		unsigned int GetAddress() const;
-		
 		unsigned char GetA() const;
-		
 		unsigned char GetB() const;
-		
 		unsigned char GetC() const;
-		
 		unsigned char GetD() const;
-		
 		unsigned short GetPort() const;
 
 	private:
