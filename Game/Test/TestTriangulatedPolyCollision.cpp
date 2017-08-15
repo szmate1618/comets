57eb5aa50cc56c23a17a1b05b997fd19c8b63bfe
Máté Szabó <szmate1618@gmail.com>
2017-08-15 23:28:19 +0200

    ignore test case Scenario01

--- Game/Test/TestTriangulatedPolyCollision.cpp
+++ Game/Test/TestTriangulatedPolyCollision.cpp
@@ -8,10 +8,15 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Test
 {		
+
 	TEST_CLASS(TestAbstractTriangulatedPolyCollision)
 	{
 	public:
 		
+		BEGIN_TEST_METHOD_ATTRIBUTE(Scenario01)
+			TEST_IGNORE()
+		END_TEST_METHOD_ATTRIBUTE()
+
 		TEST_METHOD(Scenario01)
 		{
 			Assert::Fail();
