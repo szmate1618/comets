3fbc621577e1d6a1e8b0a6782c214af55096efbb
Máté Szabó <szmate1618@gmail.com>
2017-08-15 23:01:52 +0200

    declare handle as SOCKET instead of int

--- Game/Networking/Socket.hpp
+++ Game/Networking/Socket.hpp
@@ -1,5 +1,25 @@
 #pragma once
 
+#define PLATFORM_WINDOWS  1
+#define PLATFORM_MAC      2
+#define PLATFORM_UNIX     3
+
+#if defined(_WIN32)
+#define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#define PLATFORM PLATFORM_MAC
+#else
+#define PLATFORM PLATFORM_UNIX
+#endif
+
+#if PLATFORM == PLATFORM_WINDOWS
+#include <winsock2.h>
+#elif PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#endif
+
 #include "Address.hpp"
 
 
@@ -29,7 +49,7 @@ namespace net
 
 	private:
 
-		int handle;
+		SOCKET handle;
 		bool is_open;
 
 		static void MaybeInitializeSockets(); //Only call this from constructors.
