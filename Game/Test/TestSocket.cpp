551f39a3a71826f0a17586bfb011c562d1cf2c98
Máté Szabó <szmate1618@gmail.com>
2017-08-16 12:01:56 +0200

    extend test case SendAndReceive to send a response

--- Game/Test/TestSocket.cpp
+++ Game/Test/TestSocket.cpp
@@ -21,24 +21,36 @@ namespace Test
 		TEST_METHOD(SendAndReceive)
 		{
 			net::Address a1{ 127, 0, 0, 1, 11111 }, a2{ 127, 0, 0, 1, 22222 };
-			net::Socket sender, receiver;
+			net::Socket sender, responder;
 			const char hello[] = "heLlo";
 			const char world[] = "woRld";
 
 			sender.Open(a1.GetPort());
-			receiver.Open(a2.GetPort());
+			responder.Open(a2.GetPort());
 
 			sender.Send(a2, hello, sizeof(hello));
 
 			std::this_thread::sleep_for(10ms);
 			net::Address from;
-			int bytes_read = receiver.Receive(from);
+			int bytes_read = responder.Receive(from);
 			Assert::AreNotEqual(0, bytes_read, L"Received zero bytes.");
 			Assert::IsFalse(bytes_read < 0, L"Receive failed.");
 			Assert::AreEqual(sizeof(hello), static_cast<size_t>(bytes_read), L"Sent and recevied size do not match.");
 			for (int i = 0; i < bytes_read; i++)
 			{
-				Assert::AreEqual(hello[i], receiver.recv_buffer[i], L"This character does not much, which quite likely means that none of them does.");
+				Assert::AreEqual(hello[i], responder.recv_buffer[i], L"This character does not much, which quite likely means that none of them does.");
+			}
+
+			responder.Send(a1, world, sizeof(world));
+
+			std::this_thread::sleep_for(10ms);
+			bytes_read = sender.Receive(from);
+			Assert::AreNotEqual(0, bytes_read, L"Received zero bytes.");
+			Assert::IsFalse(bytes_read < 0, L"Receive failed.");
+			Assert::AreEqual(sizeof(world), static_cast<size_t>(bytes_read), L"Sent and recevied size do not match.");
+			for (int i = 0; i < bytes_read; i++)
+			{
+				Assert::AreEqual(world[i], sender.recv_buffer[i], L"This character does not much, which quite likely means that none of them does.");
 			}
 		}
 
