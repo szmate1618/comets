973a2942088bf2c8929efb5ec259ab10ff70e86d
Máté Szabó <szmate1618@gmail.com>
2017-08-16 21:56:18 +0200

    add test case for receiving into external buffer

--- Game/Test/TestSocket.cpp
+++ Game/Test/TestSocket.cpp
@@ -20,15 +20,15 @@ namespace Test
 
 		TEST_METHOD(SendAndReceive)
 		{
-			net::Address a1{ 127, 0, 0, 1, 11111 }, a2{ 127, 0, 0, 1, 22222 };
+			net::Address address1{ 127, 0, 0, 1, 11111 }, address2{ 127, 0, 0, 1, 22222 };
 			net::Socket sender, responder;
 			const char hello[] = "heLlo";
 			const char world[] = "woRld";
 
-			sender.Open(a1.GetPort());
-			responder.Open(a2.GetPort());
+			sender.Open(address1.GetPort());
+			responder.Open(address2.GetPort());
 
-			sender.Send(a2, hello, sizeof(hello));
+			sender.Send(address2, hello, sizeof(hello));
 
 			std::this_thread::sleep_for(10ms);
 			net::Address from;
@@ -41,7 +41,7 @@ namespace Test
 				Assert::AreEqual(hello[i], responder.recv_buffer[i], L"This character does not much, which quite likely means that none of them does.");
 			}
 
-			responder.Send(a1, world, sizeof(world));
+			responder.Send(address1, world, sizeof(world));
 
 			std::this_thread::sleep_for(10ms);
 			bytes_read = sender.Receive(from);
@@ -56,37 +56,61 @@ namespace Test
 
 		TEST_METHOD(GetAddress)
 		{
-			net::Address a1{ 127, 0, 0, 1, 11111 }, a2{ 127, 0, 0, 1, 22222 };
+			net::Address address1{ 127, 0, 0, 1, 11111 }, address2{ 127, 0, 0, 1, 22222 };
 			net::Socket sender, receiver;
 			const char hello[] = "heLlo";
 
-			sender.Open(a1.GetPort());
-			receiver.Open(a2.GetPort());
+			sender.Open(address1.GetPort());
+			receiver.Open(address2.GetPort());
 
-			sender.Send(a2, hello, sizeof(hello));
+			sender.Send(address2, hello, sizeof(hello));
 
 			std::this_thread::sleep_for(10ms);
 			net::Address from;
 			int bytes_read = receiver.Receive(from);
-			Assert::AreEqual(a1.GetAddress(), from.GetAddress(), L"Addresses do not match.");
-			Assert::AreEqual(static_cast<int>(a1.GetPort()), static_cast<int>(from.GetPort()), L"Ports do not match."); //Why on Earth do I need this cast here?!
+			Assert::AreEqual(address1.GetAddress(), from.GetAddress(), L"Addresses do not match.");
+			Assert::AreEqual(static_cast<int>(address1.GetPort()), static_cast<int>(from.GetPort()), L"Ports do not match."); //Why on Earth do I need this cast here?!
 		}
 
 		TEST_METHOD(UnspecifiedPort)
 		{
-			net::Address a2{ 127, 0, 0, 1, 11111 };
+			net::Address address{ 127, 0, 0, 1, 11111 };
 			net::Socket sender, receiver;
 			const char hello[] = "heLlo";
 
 			sender.Open(0);
-			receiver.Open(a2.GetPort());
+			receiver.Open(address.GetPort());
 
-			sender.Send(a2, hello, sizeof(hello));
+			sender.Send(address, hello, sizeof(hello));
 
 			std::this_thread::sleep_for(10ms);
 			net::Address from;
 			int bytes_read = receiver.Receive(from);
 			Assert::AreNotEqual(0, static_cast<int>(from.GetPort()), L"Ports do not match.");
+		}
+
+		TEST_METHOD(ExternalBuffer)
+		{
+			net::Address address1{ 127, 0, 0, 1, 11111 }, address2{ 127, 0, 0, 1, 22222 };
+			net::Socket sender, responder;
+			const char message[] = "Mortal Wombat III";
+
+			sender.Open(address1.GetPort());
+			responder.Open(address2.GetPort());
+
+			sender.Send(address2, message, sizeof(message));
+
+			std::this_thread::sleep_for(10ms);
+			net::Address from;
+			char buffer[100];
+			int bytes_read = responder.Receive(from, buffer, sizeof(buffer));
+			Assert::AreNotEqual(0, bytes_read, L"Received zero bytes.");
+			Assert::IsFalse(bytes_read < 0, L"Receive failed.");
+			Assert::AreEqual(sizeof(message), static_cast<size_t>(bytes_read), L"Sent and recevied size do not match.");
+			for (int i = 0; i < bytes_read; i++)
+			{
+				Assert::AreEqual(message[i], buffer[i], L"This character does not much, which quite likely means that none of them does.");
+			}
 		}
 
 	};
