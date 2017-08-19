5fe7577e7d73338065750ca10c7997c9f096dfdf
Máté Szabó <szmate1618@gmail.com>
2017-08-19 14:50:59 +0200

    add TestLogger skeleton

---
+++ Game/Test/TestLogger.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "..\Utilities\Logger.hpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+
+	TEST_CLASS(Socket)
+	{
+
+		TEST_METHOD_INITIALIZE(InitializerLoggerTests)
+		{
+
+		}
+
+		TEST_METHOD_CLEANUP(CleanupLoggerTests)
+		{
+
+		}
+
+	public:
+
+		TEST_METHOD(ConstructorDestructorLogEntry)
+		{
+		}
+
+	};
+
+}
