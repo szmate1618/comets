eb4cfd6bebee0ffec851512285080ef838bde2d3
Máté Szabó <szmate1618@gmail.com>
2017-08-19 14:36:49 +0200

    improve Logger

--- Game/Utilities/Logger.hpp
+++ Game/Utilities/Logger.hpp
@@ -2,37 +2,55 @@
 
 #include <iostream>
 #include <ctime>
+#include <string>
+
 
 namespace util
 {
 	
-	enum severity { trace, debug, info, warn, error, fatal };
+	enum severity_level { trace, debug, info, warn, error, fatal };
 
 	class Logger
 	{
 
 	public:
 
-		static void Log(severity severity, const char* message)
+		Logger()
 		{
-			std::cout << std::time(nullptr) << " " << severity << " " << message << std::endl;
+			std::ios_base::Init initialize_stream;
+			std::ios_base::sync_with_stdio(false);
+			(*this)(info, "Logger initialized with sync_with_stdio(false).");
+		}
+
+		~Logger()
+		{
+			(*this)(info, "Logger destructed.");
+		}
+
+		void operator()(const severity_level severity, std::string message) const
+		{
+			const char * severity_text;
+			switch (severity)
+			{
+			case trace: {severity_text = "trace"; break; }
+			case debug: {severity_text = "debug"; break; }
+			case info: {severity_text = "info_"; break; }
+			case warn: {severity_text = "warn_"; break; }
+			case error: {severity_text = "error"; break; }
+			case fatal: {severity_text = "fatal"; break; }
+			default:
+			{
+				severity_text = "?????";
+				(*this)(warn, std::string("Unkown severity level: ") + std::to_string(severity) + std::string("."));
+				break;
+			}
+			}
+			std::cout << std::time(nullptr) << "\t" << severity_text << "\t" << message << std::endl;
 			std::cout.flush();
 		}
 
-	private:
-
-		class Initializer
-		{
-		public:
-			Initializer()
-			{ 
-				std::ios_base::sync_with_stdio(false);
-				Log(info, "Logger initialized with sync_with_stdio(false).");
-			}
-		};
-
-		static Initializer initialize;
-
 	};
+
+	Logger Log;
 
 }
