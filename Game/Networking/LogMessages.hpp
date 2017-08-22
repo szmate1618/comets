a0ad5182d5f89cd5adbdbc0f1aa5605c3d699bbb
Máté Szabó <szmate1618@gmail.com>
2017-08-22 22:10:29 +0200

    update LogMessages

--- Game/Networking/LogMessages.hpp
+++ Game/Networking/LogMessages.hpp
@@ -4,9 +4,10 @@
 namespace net
 {
 
-	static const char* const unknown_error = "Sorry, this errorcode is totally unknown. Its numerical value is ";
+	static const char* const unregistered_error = "Sorry, this errorcode is not registered. Maybe look it up in the manual. Its numerical value is ";
 
-
+	static const char* const blocking_mode_fail = "Failed to set non-blocking mode.";
+	static const char* const packet_send_fail = "Failed to send packet.";
 	static const char* const socket_bind_fail = "Failed to bind socket. See the details below then consult the manual of your socket library.";
 
 	static const char* const WSANOTINITIALISED_text = "WSANOTINITIALISED: A successful WSAStartup call must occur before using this function.";
@@ -18,6 +19,6 @@ namespace net
 	static const char* const WSAEINPROGRESS_text = "WSAEINPROGRESS: A blocking Windows Sockets 1.1 call is in progress, or the service provider is still processing a callback function.";
 	static const char* const WSAEINVAL_text = "WSAEINVAL: An invalid argument was supplied. This error is returned of the socket s is already bound to an address.";
 	static const char* const WSAENOBUFS_text = "WSAENOBUFS: An operation on a socket could not be performed because the system lacked sufficient buffer space or because a queue was full.";
-	static const char* const WSAENOTSOCK_text = "WSAENOTSOCK: An operation was attempted on something that is not a socket. Did you try to bind a closed one?";
+	static const char* const WSAENOTSOCK_text = "WSAENOTSOCK: An operation was attempted on something that is not a socket. Are you trying to bind a closed one?";
 
 }
