69f67d267e356d6b92c7ff5082938e9505cc7030
Máté Szabó <szmate1618@gmail.com>
2017-10-18 10:27:23 +0200

    fix main loop

--- Game/Server/main.cpp
+++ Game/Server/main.cpp
@@ -14,9 +14,9 @@ int main() //TODO: Maybe factor this out into a class for better testability.
 	while (running && gameserver->Running())
 	{
 		auto start = std::chrono::steady_clock::now();
-		std::this_thread::sleep_for(server::tick{ 1 });
+		gameserver->Tick(server::tick{ 1 });
 		auto end = std::chrono::steady_clock::now();
-		gameserver->Tick(end - start);
+		std::this_thread::sleep_for(server::tick{ 1 } -  (end - start)); //TODO: What happens if this is negative?
 	}
 
 	return 0;
