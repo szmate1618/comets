8aaefcc50335cf4bf1c36b9dee1a4eb4904839eb
Máté Szabó <szmate1618@gmail.com>
2017-10-20 09:00:53 +0200

    nit

--- Game/Server/main.cpp
+++ Game/Server/main.cpp
@@ -16,7 +16,7 @@ int main() //TODO: Maybe factor this out into a class for better testability.
 		auto start = std::chrono::steady_clock::now();
 		gameserver->Tick(server::tick{ 1 });
 		auto end = std::chrono::steady_clock::now();
-		std::this_thread::sleep_for(server::tick{ 1 } -  (end - start)); //TODO: What happens if this is negative?
+		std::this_thread::sleep_for(server::tick{ 1 } - (end - start)); //TODO: What happens if this is negative?
 	}
 
 	return 0;
