ecc3296d5b150545edba5e7ae5a2d840b05e33c7
Máté Szabó <szmate1618@gmail.com>
2017-10-21 18:55:20 +0200

    add protocol parameter to GameServer constructor, add important C++ theoretical comment

--- Game/Server/GameServer.cpp
+++ Game/Server/GameServer.cpp
@@ -1,10 +1,20 @@
 #include "GameServer.hpp"
 
+#include <cassert>
+
 
 namespace server
 {
 
-	GameServer::GameServer() : running{ true }, input_thread{ [=] { ReadPackets(); } }
+	//Stroustrup: "The constructors are called in the order in which the members are declared
+	//in the class rather than the order in which the members appear in the initializer list.
+	//To avoid confusion, it is best to specify the initializers in the member declaration order."
+	GameServer::GameServer() :
+		export_strategy{ entity_buffer, client_input_buffer },
+		import_strategy{ server_state_buffer },
+		protocol{ 0, export_strategy, import_strategy },
+		running{ true },
+		input_thread{ [=] { ReadPackets(); } }
 	{
 	}
 
@@ -52,5 +62,24 @@ namespace server
 	void GameServer::SendPackets()
 	{
 	}
+
+	GameServer::ExportStrategy::ExportStrategy(std::vector<def::entity_id>& eb, std::vector<net::ClientIntputPayload>& cib) {};
+	GameServer::ExportStrategy::~ExportStrategy() {};
+	void GameServer::ExportStrategy::Export(const net::ServerStatePayload&) { assert(false && "Not actually implemented, not supposed to be called."); };
+	void GameServer::ExportStrategy::Export(def::entity_id, const net::ClientIntputPayload&) {}; //TODO: Actually implement this.
+
+	GameServer::ImportStrategy::ImportStrategy(std::vector<net::ServerStatePayload>& ssb) {};
+	GameServer::ImportStrategy::~ImportStrategy() {};
+	std::tuple<size_t, def::entity_id*, net::ServerStatePayload*> GameServer::ImportStrategy::ImportServerState()
+	{
+		std::tuple<size_t, def::entity_id*, net::ServerStatePayload*> dummy_return;
+		return dummy_return;
+	}; //TODO: Actually implement this.
+	std::tuple<size_t, net::ClientIntputPayload*> GameServer::ImportStrategy::ImportClientIntput() 
+	{
+		assert(false && "Not actually implemented, not supposed to be called.");
+		std::tuple<size_t, net::ClientIntputPayload*> dummy_return;
+		return dummy_return;
+	};
 
 }
--- Game/Server/GameServer.hpp
+++ Game/Server/GameServer.hpp
@@ -2,10 +2,12 @@
 
 #include "AbstractServer.hpp"
 #include "..\Networking\Protocol.hpp"
+#include "..\Networking\Packets.hpp"
 #include "..\Definitions\TimeAndNetwork.hpp"
 
 #include <chrono>
 #include <thread>
+#include <vector>
 
 
 namespace server
@@ -24,6 +26,34 @@ namespace server
 
 	private:
 
+		class ExportStrategy : public net::AbstractExportStrategy
+		{
+		public:
+
+			ExportStrategy(std::vector<def::entity_id>&, std::vector<net::ClientIntputPayload>&);
+			virtual ~ExportStrategy() override;
+			virtual void Export(const net::ServerStatePayload&) override;
+			virtual void Export(def::entity_id, const net::ClientIntputPayload&) override;
+
+		};
+
+		class ImportStrategy : public net::AbstractImportStrategy
+		{
+		public:
+
+			ImportStrategy(std::vector<net::ServerStatePayload>&);
+			virtual ~ImportStrategy() override;
+			virtual std::tuple<size_t, def::entity_id*, net::ServerStatePayload*> ImportServerState() override;
+			virtual std::tuple<size_t, net::ClientIntputPayload*> ImportClientIntput() override;
+
+		};
+
+		std::vector<def::entity_id> entity_buffer;
+		std::vector<net::ClientIntputPayload> client_input_buffer;
+		std::vector<net::ServerStatePayload> server_state_buffer;
+		ExportStrategy export_strategy;
+		ImportStrategy import_strategy;
+		net::ServersideProtocol protocol;
 		bool running;
 		std::thread input_thread;
 
@@ -32,6 +62,7 @@ namespace server
 		void UpdateState(def::time);
 		void TestCollisions();
 		void SendPackets();
+
 	};
 
 }
