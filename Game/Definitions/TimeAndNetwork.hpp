c8ec9c2498eb984c10bb3b8a708ea49ef81e56f9
Máté Szabó <szmate1618@gmail.com>
2017-10-27 09:55:40 +0200

    nit on time and network definitions

--- Game/Definitions/TimeAndNetwork.hpp
+++ Game/Definitions/TimeAndNetwork.hpp
@@ -7,21 +7,26 @@
 namespace def
 {
 
+	//Time type defnitions
 	using namespace std::chrono_literals;
 	using time = std::chrono::duration<double>;
 	using server_tick = std::chrono::duration<double, std::ratio<1, 90>>;
 
+	//Time constants
 	static constexpr time connection_timeout = 5s;
 	static constexpr time max_sleep = 1s; //TODO: Clean this up. Do we want to use this? Or we prefer max_socket_read_backoff?
 	static constexpr time max_socket_read_backoff = 10ms;
 	static constexpr time server_rate = server_tick(1);
 	static constexpr time client_rate = server_tick(2);
 
+	//Socket parameters
 	static constexpr int max_packet_size = 1000; //TODO: Check recommended max MTU.
 
+	//Protocol parameters
 	using entity_id = long;
 	static constexpr uint8_t protocol_id = 77;
 
+	//Server configuration
 	static constexpr unsigned short server_port = 7700; //TODO: This should be specified in a config file. Maybe the others, too.
 
 }
