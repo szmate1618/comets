9d348bfa2ca484e5806c0635803335575f449b2f
Máté Szabó <szmate1618@gmail.com>
2017-11-02 10:47:11 +0100

    add TestServersideProtocol stub

---
+++ Game/Test/TestServerSideProtocol.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "..\Networking\Protocol.hpp"
+#include "..\Server\GameServer.hpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+
+	TEST_CLASS(ServerSideProtocol)
+	{
+
+	public:
+
+		TEST_METHOD(Test01)
+		{
+		}
+
+	};
+
+}
