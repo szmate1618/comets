8be8204923dc3545a449f153675cfa1b832e373f
Máté Szabó <szmate1618@gmail.com>
2017-11-12 00:31:17 +0100

    add TODO

--- Game/Utilities/CountOfArray.hpp
+++ Game/Utilities/CountOfArray.hpp
@@ -4,6 +4,7 @@
 namespace util
 {
 
+	//TODO: Or maybe just use std::array everywhere?
 	template<typename T, size_t N>
 	constexpr size_t countof(const T (&array)[N]) { return N; } //TODO: Add test.
 
