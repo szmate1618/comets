38b3b3152a623589e2d8e3e0299d14b12e25f781
Máté Szabó <szmate1618@gmail.com>
2018-02-16 01:34:53 +0100

    add Universe.cpp skeleton

---
+++ Game/Entities/Universe.cpp
@@ -0,0 +1,38 @@
+#include "Universe.hpp"
+
+
+namespace entity
+{
+
+	Universe::Universe() {}
+
+	Universe::Universe(std::string) {} //TODO: Implement this.
+
+	Universe::~Universe() {}
+
+	bool Universe::EntityTurnLeft(def::time duration, def::entity_id entity_id)
+	{
+
+	}
+
+	bool Universe::EntityTurnRight(def::time duration, def::entity_id entity_id)
+	{
+
+	}
+
+	bool Universe::EntityThrust(def::time duration, def::entity_id entity_id)
+	{
+
+	}
+
+	bool Universe::EntityBrake(def::time duration, def::entity_id entity_id)
+	{
+
+	}
+
+	bool Universe::EntityFire(def::time duration, def::entity_id entity_id)
+	{
+
+	}
+
+}
