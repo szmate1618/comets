c014945ee79c4a8c0fc18cef13bad6fee34a7df8
Máté Szabó <szmate1618@gmail.com>
2018-03-06 00:03:27 +0100

    update AbstractPartitioner.hpp

--- Game/Entities/AbstractPartitioner.hpp
+++ Game/Entities/AbstractPartitioner.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Entities.hpp"
-#include "..\Geometry\Geometry.hpp"
 #include "Iterator.hpp"
 #include "AbstractPartition.hpp"
 
@@ -11,13 +10,27 @@ namespace entity
 
 	class AbstractPartitioner
 	{
+
+		class AbstractIterator
+		{
+		public:
+
+			virtual ~AbstractIterator() = 0;
+			virtual AbstractIterator& operator++() = 0;
+			virtual bool operator==(const AbstractIterator&) const = 0;
+			virtual bool operator!=(const AbstractIterator&) const = 0;
+			virtual AbstractPartition& operator*() const = 0;
+		};
+
 	public:
 
 		AbstractPartitioner();
 		virtual ~AbstractPartitioner();
-		virtual void Insert(geo::Frame<StaticEntity>&) = 0;
-		virtual Iterator<AbstractPartition* const> begin() = 0;
-		virtual Iterator<AbstractPartition* const> end() = 0;
+		virtual void Insert(StaticEntity* const) = 0; //TODO: Include an EmptyFrame in AbstractCollisionShape.
+
+		//These consts are of utmost importance, see here: https://herbsutter.com/2008/01/01/gotw-88-a-candidate-for-the-most-important-const/
+		virtual const AbstractIterator& begin() = 0;
+		virtual const AbstractIterator& end() = 0;
 
 	private:
 	};
