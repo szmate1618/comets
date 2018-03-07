591fddf4a5ad910ece28c4b40764a028e82936c0
Máté Szabó <szmate1618@gmail.com>
2018-03-07 22:46:41 +0100

    actually add collision check

--- Game/Entities/AbstractCollisionShape.hpp
+++ Game/Entities/AbstractCollisionShape.hpp
@@ -21,6 +21,7 @@ namespace entity
 		virtual bool InviteForCollision(geo::EmptyFrame, geo::EmptyFrame, AbstractCollisionShape&) = 0;
 		virtual bool CollideInto(geo::EmptyFrame, geo::EmptyFrame, AbstractTriangulatedPoly&) = 0;
 		virtual bool CollideInto(geo::EmptyFrame, geo::EmptyFrame, Circle&) = 0;
+		virtual const geo::EmptyFrame& GetBoundingBox() = 0;
 		virtual std::string GetName() = 0; //Useful for debugging.
 
 	private:
