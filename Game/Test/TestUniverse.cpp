21c1a580f1dfe58dacac5795df25c1261d137540
Máté Szabó <szmate1618@gmail.com>
2018-03-22 11:19:59 +0100

    add TestUniverse skeleton

---
+++ Game/Test/TestUniverse.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "..\Entities\Universe.hpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test
+{
+
+	TEST_CLASS(TestUniverse)
+	{
+	public:
+
+		TEST_METHOD(ConstructFromFile)
+		{
+		}
+	};
+
+}
