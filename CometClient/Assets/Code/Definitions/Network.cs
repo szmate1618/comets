f4da5bb7acb68361a3da7a782d9e1b26a05471e3
Máté Szabó <szmate1618@gmail.com>
2018-06-25 00:56:45 +0200

    add Definitions/Network.cs, and corresponding .meta files

---
+++ CometClient/Assets/Code/Definitions/Network.cs
@@ -0,0 +1,19 @@
+﻿//TODO: Unify this with the C++ version, as much as possible.
+using System;
+
+namespace def
+{
+
+    static class Network
+    {   
+
+        //Socket parameters
+        public const int MaxPacketSize = 1000; //TODO: Check recommended max MTU.
+
+        //Protocol parameters
+        public const byte ProtocolId = 77;
+        public enum user_input { turn_left, turn_right, thrust, brake, fire };
+
+    }
+
+}
