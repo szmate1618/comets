9aafe931d24dcaf350b2e13f682f98576104be64
Máté Szabó <szmate1618@gmail.com>
2018-06-27 23:55:12 +0200

    don't create temporary GameServer object

--- CometServer/Server/main.cpp
+++ CometServer/Server/main.cpp
@@ -7,14 +7,14 @@
 
 int main() //TODO: Maybe factor this out into a class for better testability.
 {
-	server::AbstractServer* gameserver = &server::GameServer{};
+	server::GameServer gameserver{};
 	
 	volatile bool running = true;
 
-	while (running && gameserver->Running())
+	while (running && gameserver.Running())
 	{
 		auto start = std::chrono::steady_clock::now();
-		gameserver->Tick(server::tick{ 1 });
+		gameserver.Tick(server::tick{ 1 });
 		auto end = std::chrono::steady_clock::now();
 		std::this_thread::sleep_for(server::tick{ 1 } - (end - start)); //TODO: What happens if this is negative?
 	}
