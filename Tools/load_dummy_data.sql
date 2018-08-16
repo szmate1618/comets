d239f4e2d3422def288663c66e5feceab16d7695
Máté Szabó <szmate1618@gmail.com>
2018-08-16 15:07:35 +0200

    add load_dummy_data.sql and .bat

---
+++ Tools/load_dummy_data.sql
@@ -0,0 +1,18 @@
+BEGIN TRANSACTION;
+
+INSERT INTO Users VALUES(0, "placeholder");
+INSERT INTO Users VALUES(1, "placeholder");
+INSERT INTO Users VALUES(2, "placeholder");
+INSERT INTO Users VALUES(3, "placeholder");
+INSERT INTO Users VALUES(4, "placeholder");
+
+INSERT INTO Hulls VALUES(0, "placeholder", "placeholder");
+
+INSERT INTO Entities VALUES(0, 1, 0, "inertial", "dynamic", "visible", "collidable", 0, 0, 0);
+INSERT INTO Entities VALUES(1, 2, 0, "inertial", "dynamic", "visible", "collidable", 10, 0, 0);
+INSERT INTO Entities VALUES(2, 3, 0, "inertial", "dynamic", "visible", "collidable", 10, 10, 0);
+INSERT INTO Entities VALUES(3, 4, 0, "inertial", "dynamic", "visible", "collidable", 0, 10, 0);
+INSERT INTO Entities VALUES(4, 1, 0, "anti_inertial", "dynamic", "visible", "collidable", -10, 0, 0);
+INSERT INTO Entities VALUES(5, 1, 0, "para_inertial", "dynamic", "visible", "collidable", 0, -10, 0);
+
+COMMIT;
