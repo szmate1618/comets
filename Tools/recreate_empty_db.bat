1dac161bae1ab5d74f0d7cb79ce2621a69b0ca95
Máté Szabó <szmate1618@gmail.com>
2018-08-16 14:17:52 +0200

    add recreate_empty_db.sql and .bat

---
+++ Tools/recreate_empty_db.bat
@@ -0,0 +1,1 @@
+sqlite3_x86.exe ..\CometServer\Server\game_data.sqlite ".read recreate_empty_db.sql"
---
+++ Tools/recreate_empty_db.sql
@@ -0,0 +1,36 @@
+PRAGMA encoding=utf8;
+PRAGMA foreign_keys=ON;
+
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users
+(
+	UserID INTEGER PRIMARY KEY ASC,
+	PasswordHash TEXT
+);
+
+DROP TABLE IF EXISTS Hulls;
+CREATE TABLE Hulls
+(
+	HullID INTEGER PRIMARY KEY ASC,
+	shape TEXT, texture TEXT
+);
+
+DROP TABLE IF EXISTS Entities;
+CREATE TABLE Entities
+(
+	EntityID INTEGER PRIMARY KEY ASC,
+	OwnerID references Users(UserID),
+	HullID references Hulls(HullID),
+	Engine TEXT, --inertial, anti_intertial, para_inertial, pre_programmed
+	Dynamics TEXT, --static_, dynamic
+	Visibility TEXT, --visible, invisible
+	Collidability TEXT, --collidable, uncollidable
+	PositionX REAL,
+	PositionY REAL,
+	Orientation REAL
+	--TODO: Also add velocity, acceleration, friciton, etc.
+);
+
+COMMIT;
