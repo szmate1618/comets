e82a987f7086133a434e61628742adf0ba2a9d79
Máté Szabó <szmate1618@gmail.com>
2018-09-24 18:50:36 +0200

    add rudimentary fisheye effect

---
+++ CometClient/Assets/Code/Camera/MainCameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+
+	public Shader fishEyeShader;
+
+	private void Start ()
+	{
+		/*Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+		var asd = GetComponent<Camera>().cullingMatrix;
+		GetComponent<Camera>().cullingMatrix = Matrix4x4.Frustum(-10.0f, 10.0f, -10.0f, 11.0f, 0.3f, 110.0f);
+		//fishEyeShader = Shader.Find("FishEye");
+		Plane[] planes2 = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+		var qwe = GetComponent<Camera>().cullingMatrix;*/
+		GetComponent<Camera>().SetReplacementShader(fishEyeShader, "");
+	}
+
+	void OnPreCull()
+	{
+		Camera.main.cullingMatrix = Matrix4x4.Ortho(-99999, 99999, -99999, 99999, 0.001f, 99999) *
+							Matrix4x4.Translate(Vector3.forward * -99999 / 2f) *
+							Camera.main.worldToCameraMatrix;
+	}
+
+		/*private void OnRenderImage(RenderTexture src, RenderTexture dst)
+		{
+			//fishEyeShader = Shader.Find("FishEye");
+			var cam = GetComponent<Camera>();//.SetReplacementShader(fishEyeShader, "");
+			Graphics.Blit(src, dst, new Material(fishEyeShader));
+		}*/
+
+	}
