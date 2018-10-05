3744e0b94136af81e1f9d4fd71396920f9715ae2
Máté Szabó <szmate1618@gmail.com>
2018-10-05 17:10:44 +0200

    customize script to suit my needs

--- Tools/export_shape.py
+++ Tools/export_shape.py
@@ -1,10 +1,10 @@
 import bpy
 
 
-def write_some_data(context, filepath, use_some_setting):
-    print("running write_some_data...")
+def export_shape(context, filepath):
+    print("running export_shape...")
     f = open(filepath, 'w', encoding='utf-8')
-    f.write("Hello World %s" % use_some_setting)
+    f.write("Hello World")
     f.close()
 
     return {'FINISHED'}
@@ -17,52 +17,36 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 
-class ExportSomeData(Operator, ExportHelper):
-    """This appears in the tooltip of the operator and in the generated docs"""
-    bl_idname = "export_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Export Some Data"
+class ShapeExporter(Operator, ExportHelper):
+    """Export the active object as a Comet Shape file"""
+    bl_idname = "export_mesh.shape"  # important since its how bpy.ops.import_mesh.shape is constructed
+    bl_label = "Export Shape"
 
     # ExportHelper mixin class uses this
-    filename_ext = ".txt"
+    filename_ext = ".shp"
 
     filter_glob = StringProperty(
-            default="*.txt",
+            default="*.shp",
             options={'HIDDEN'},
             maxlen=255,  # Max internal buffer length, longer would be clamped.
             )
 
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
-    use_setting = BoolProperty(
-            name="Example Boolean",
-            description="Example Tooltip",
-            default=True,
-            )
-
-    type = EnumProperty(
-            name="Example Enum",
-            description="Choose between two items",
-            items=(('OPT_A', "First Option", "Description one"),
-                   ('OPT_B', "Second Option", "Description two")),
-            default='OPT_A',
-            )
-
     def execute(self, context):
-        return write_some_data(context, self.filepath, self.use_setting)
+        return export_shape(context, self.filepath)
 
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(ExportSomeData.bl_idname, text="Text Export Operator")
+    self.layout.operator(ShapeExporter.bl_idname, text="Shape (.shp)")
 
 
 def register():
-    bpy.utils.register_class(ExportSomeData)
+    bpy.utils.register_class(ShapeExporter)
     bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_class(ExportSomeData)
+    bpy.utils.unregister_class(ShapeExporter)
     bpy.types.INFO_MT_file_export.remove(menu_func_export)
 
 
@@ -70,4 +54,4 @@ if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.export_test.some_data('INVOKE_DEFAULT')
+    bpy.ops.export_mesh.shape('INVOKE_DEFAULT')
