83ced38093e1e979f34f303b2115765a4b096f8c
Máté Szabó <szmate1618@gmail.com>
2018-10-09 19:34:58 +0200

    add generate UVs

--- Tools/export_shape.py
+++ Tools/export_shape.py
@@ -11,15 +11,26 @@ def export_shape(context, filepath):
     mesh = object.data
     with open(filepath, 'w', encoding='utf-8') as f:
         f.write("{} {}\n".format(len(mesh.vertices), len(mesh.polygons)))
+        #Print vertices.
         for v in mesh.vertices:
             assert len(v.co) == 3, "Coordinate is not XYZ."
             f.write("{} {}\n".format(v.co[0], v.co[1])) #Z coordinate is implicitly 0.
+        #Print UV coordinates.
         if mesh.uv_layers.active: #If there is a UV mapping specified.
             uvs = {mesh.loops[l].vertex_index: mesh.uv_layers.active.data[l].uv for p in mesh.polygons for l in p.loop_indices}
             for v in mesh.vertices:
                 f.write("{} {}\n".format(uvs[v.index][0], uvs[v.index][1]))
         else: #If there is no UV mapping specified.
-            pass
+            x_coordinates = [v.co[0] for v in mesh.vertices]
+            y_coordinates = [v.co[1] for v in mesh.vertices]
+            min_x = min(x_coordinates)
+            max_x = max(x_coordinates)
+            min_y = min(y_coordinates)
+            max_y = max(y_coordinates)
+            max_diff = max_x - min_x if max_x - min_x > max_y - min_y else max_y - min_y
+            for v in mesh.vertices:
+                f.write("{} {}\n".format((v.co[0] - min_x) / max_diff, (v.co[1] - min_y) / max_diff))
+        #Print triangles.
         for p in mesh.polygons:
             assert len(p.vertices) == 3, "Mesh is not triangulated."
             f.write("{} {} {}\n".format(p.vertices[0], p.vertices[1], p.vertices[2]))
