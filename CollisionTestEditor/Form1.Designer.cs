2d715f9181042db553df303e976363de7c93c250
Máté Szabó <szmate1618@gmail.com>
2019-08-23 20:32:56 +0200

    add controls for first shape

--- CollisionTestEditor/Form1.Designer.cs
+++ CollisionTestEditor/Form1.Designer.cs
@@ -28,20 +28,167 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label2 = new System.Windows.Forms.Label();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.label3 = new System.Windows.Forms.Label();
+			this.trackBar4 = new System.Windows.Forms.TrackBar();
+			this.label4 = new System.Windows.Forms.Label();
+			this.trackBar5 = new System.Windows.Forms.TrackBar();
+			this.label5 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 25);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Load shape";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(26, 25);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "φ";
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(54, 43);
+			this.trackBar1.Maximum = 360;
+			this.trackBar1.Minimum = -360;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(100, 56);
+			this.trackBar1.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(14, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(34, 25);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "[x]";
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.Location = new System.Drawing.Point(54, 82);
+			this.trackBar2.Maximum = 100;
+			this.trackBar2.Minimum = -100;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(100, 56);
+			this.trackBar2.TabIndex = 4;
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.Location = new System.Drawing.Point(204, 82);
+			this.trackBar3.Maximum = 100;
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Size = new System.Drawing.Size(100, 56);
+			this.trackBar3.TabIndex = 6;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(164, 82);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(36, 25);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "{x}";
+			// 
+			// trackBar4
+			// 
+			this.trackBar4.Location = new System.Drawing.Point(204, 135);
+			this.trackBar4.Maximum = 100;
+			this.trackBar4.Name = "trackBar4";
+			this.trackBar4.Size = new System.Drawing.Size(100, 56);
+			this.trackBar4.TabIndex = 10;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(164, 135);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(36, 25);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "{y}";
+			// 
+			// trackBar5
+			// 
+			this.trackBar5.Location = new System.Drawing.Point(54, 135);
+			this.trackBar5.Maximum = 100;
+			this.trackBar5.Minimum = -100;
+			this.trackBar5.Name = "trackBar5";
+			this.trackBar5.Size = new System.Drawing.Size(100, 56);
+			this.trackBar5.TabIndex = 8;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(14, 135);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(34, 25);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "[y]";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(982, 703);
+			this.Controls.Add(this.trackBar4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.trackBar5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.trackBar3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "CollisionTestEditor";
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TrackBar trackBar4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TrackBar trackBar5;
+		private System.Windows.Forms.Label label5;
 	}
 }
 
