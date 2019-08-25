a4af259069d82ac21cce7dcbaeb8a8299216f7f2
Máté Szabó <szmate1618@gmail.com>
2019-08-25 21:27:22 +0200

    add Shape.cs

---
+++ CollisionTestEditor/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CollisionTestEditor
+{
+	class Shape
+	{
+
+		string description;
+		double orientation;
+		ModelPoint position;
+		List<ModelPoint> vertices;
+		List<int> triangles;
+
+	}
+}
