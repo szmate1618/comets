f5216a8a946423d546441c0aba32c0507d3301ec
Máté Szabó <szmate1618@gmail.com>
2019-08-27 20:55:31 +0200

    construct poly shape

--- CollisionTestEditor/Shape.cs
+++ CollisionTestEditor/Shape.cs
@@ -1,19 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace CollisionTestEditor
 {
 	class Shape
 	{
-		string description;
-		double orientation;
-		ModelPoint position;
-		List<ModelPoint> vertices;
-		List<int> triangles;
-		bool is_circle;
+		private string description;
+		private List<ModelPoint> vertices;
+		private List<int> triangles;
+		private bool is_circle;
+
+		public double orientation;
+		public ModelPoint position;
 
 		public Shape(string description)
 		{
 			this.description = description;
+
+			StringReader sr = new StringReader(description);
+
+			string VT = sr.ReadLine();
+			int vertex_count = Convert.ToInt32(VT.Split()[0]);
+			int triangle_count = Convert.ToInt32(VT.Split()[1]);
+			if (!VT.StartsWith("2 1"))
+			{
+				is_circle = false;
+				vertices = sr.ReadLine()
+					.Split()
+					.Select((c, i) => new { Coord = Convert.ToDouble(c), Index = i })
+					.GroupBy(x => (int)(x.Index / 2))
+					.Select(g => new ModelPoint(g.ToList()[0].Coord, g.ToList()[1].Coord))
+					.ToList();
+				sr.ReadLine();
+				triangles = sr.ReadLine().Split().Select(i => Convert.ToInt32(i)).ToList();
+			}
+			else
+			{
+				is_circle = true;
+			}
 		}
 	}
 }
