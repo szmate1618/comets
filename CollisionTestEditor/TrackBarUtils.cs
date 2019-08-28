db0e35280986651014b8d5457074c74b38239f1e
Máté Szabó <szmate1618@gmail.com>
2019-08-28 19:35:32 +0200

    add TrackBarUtils

---
+++ CollisionTestEditor/TrackBarUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CollisionTestEditor
+{
+	static class TrackBarUtils
+	{
+		public static double GetRadian(TrackBar trackBar)
+		{
+			return trackBar.Value / 180.0 * Math.PI;
+		}
+
+		public static double GetCoordinate(TrackBar integerPart, TrackBar fractionalPart)
+		{
+			return integerPart.Value + ((integerPart.Value > 0) ? 0.01 : -0.01) * fractionalPart.Value;
+		}
+	}
+}
