bb69874351b18cefb3534dff9e3bb36e7b86d40d
Máté Szabó <szmate1618@gmail.com>
2019-09-25 09:50:14 +0200

    add asteroid shapes to dummy configuration

--- Tools/load_dummy_data.sql
+++ Tools/load_dummy_data.sql
@@ -19,6 +19,9 @@ INSERT INTO Entities
 (4,			1,			0,			0,			'anti_inertial','dynamic',	'visible',	'collidable',	-10,		0,			0),
 (5,			1,			3,			0,			'para_inertial','dynamic',	'visible',	'collidable',	0,			-10,		0),
 (6,			1,			5,			0,			'inertial',		'dynamic',	'visible',	'collidable',	20,			0,			0),
-(7,			2,			5,			0,			'inertial',		'dynamic',	'visible',	'collidable',	25,			0,			0);
+(7,			2,			5,			0,			'inertial',		'dynamic',	'visible',	'collidable',	25,			0,			0),
+(8,			0,			6,			0,			'inertial',		'dynamic',	'visible',	'collidable',	45,			-20,		0),
+(9,			0,			7,			0,			'inertial',		'dynamic',	'visible',	'collidable',	45,			0,			0),
+(10,		0,			8,			0,			'inertial',		'dynamic',	'visible',	'collidable',	45,			20,			0);
 
 COMMIT;
