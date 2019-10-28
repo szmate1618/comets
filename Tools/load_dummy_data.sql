7ebaab456158aa409580015a128afc66017f0e3a
Máté Szabó <szmate1618@gmail.com>
2019-10-28 20:35:13 +0100

    add comet buster texture

--- Tools/load_dummy_data.sql
+++ Tools/load_dummy_data.sql
@@ -18,9 +18,9 @@ INSERT INTO CollisionBehaviors
 
 INSERT INTO Entities
 (EntityID,	OwnerID,	ShapeID,	TextureID,	Engine,				Dynamics,	Visibility,	Collidability,	PositionX,	PositionY,	Orientation) VALUES
-(0,			1,			2,			1,			'inertial',			'dynamic',	'visible',	'collidable',	0,			0,			0),
+(0,			1,			2,			8,			'inertial',			'dynamic',	'visible',	'collidable',	0,			0,			0),
 (1,			2,			1,			2,			'inertial',			'dynamic',	'visible',	'collidable',	10,			0,			0),
-(2,			3,			2,			1,			'inertial',			'dynamic',	'visible',	'collidable',	10,			10,			0),
+(2,			3,			2,			8,			'inertial',			'dynamic',	'visible',	'collidable',	10,			10,			0),
 (3,			4,			0,			1,			'inertial',			'dynamic',	'visible',	'collidable',	0,			10,			0),
 (4,			1,			0,			1,			'anti_inertial',	'dynamic',	'visible',	'collidable',	-10,		0,			0),
 (5,			1,			3,			3,			'para_inertial',	'dynamic',	'visible',	'collidable',	0,			-10,		0),
