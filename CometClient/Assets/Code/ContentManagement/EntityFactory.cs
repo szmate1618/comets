1a5fd60196464c8333783023732017cf5ca91a16
Máté Szabó <szmate1618@gmail.com>
2019-11-02 16:02:41 +0100

    add EntityFactory

---
+++ CometClient/Assets/Code/ContentManagement/EntityFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EntityFactory
+{
+
+	public GameObject Create(net.ShapeDescription shapeDescription)
+	{
+		return null;
+	}
+}
