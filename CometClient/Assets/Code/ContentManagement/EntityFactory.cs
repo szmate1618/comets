024689ea03c6ae4a6aaa2e5fc4652b90ff02b68f
Máté Szabó <szmate1618@gmail.com>
2019-12-27 18:01:57 +0100

    simplify initialization, according to Visual Studio's C# code style suggestion

--- CometClient/Assets/Code/ContentManagement/EntityFactory.cs
+++ CometClient/Assets/Code/ContentManagement/EntityFactory.cs
@@ -18,8 +18,10 @@ public class EntityFactory
 		entity.GetComponent<MeshFilter>().mesh = MeshFactory.Create(shapeDescription.vertex_count, shapeDescription.triangle_count,
 			shapeDescription.vertices, shapeDescription.uvs, shapeDescription.triangles);
 		//TODO: Some kind of default material should be used here, so the fisheye could be turned off if desired.
-		entity.GetComponent<MeshRenderer>().material = new Material(Resources.Load<Shader>("Fisheye"));
-		entity.GetComponent<MeshRenderer>().material.mainTexture = textureManager.GetTexture(shapeDescription.texture_id);
+		entity.GetComponent<MeshRenderer>().material = new Material(Resources.Load<Shader>("Fisheye"))
+		{
+			mainTexture = textureManager.GetTexture(shapeDescription.texture_id)
+		};
 		entity.AddComponent<entity.EntityController>();
 		return entity;
 	}
