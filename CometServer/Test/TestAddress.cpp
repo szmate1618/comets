a74f07edb0316b612c8b31caa82566bc7868790a
Máté Szabó <szmate1618@gmail.com>
2019-12-27 23:42:30 +0100

    Revert "intentionally make a unit test fail to test our new GitHub Actions CI system"
    This reverts commit a3f9f9df4ed436c50b48fb8ebd3d3e95f1172545.

--- CometServer/Test/TestAddress.cpp
+++ CometServer/Test/TestAddress.cpp
@@ -17,7 +17,7 @@ namespace Test
 		{
 			net::Address address{ 0xAA, 0xBB, 0xCC, 0xDD, 11213 };
 			Assert::AreEqual(0xAABBCCDD, address.GetAddress(), L"Icorrect address.");
-			Assert::AreEqual(0xFF, static_cast<int>(address.GetA()), L"Incorrect A value.");
+			Assert::AreEqual(0xAA, static_cast<int>(address.GetA()), L"Incorrect A value.");
 			Assert::AreEqual(0xBB, static_cast<int>(address.GetB()), L"Incorrect B value.");
 			Assert::AreEqual(0xCC, static_cast<int>(address.GetC()), L"Incorrect C value.");
 			Assert::AreEqual(0xDD, static_cast<int>(address.GetD()), L"Incorrect D value.");
