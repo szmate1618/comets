feb9ec5e6933070c73c47cc46fe0a44407f2127c
Máté Szabó <szmate1618@gmail.com>
2019-12-27 18:06:27 +0100

    nit

--- CometClient/Assets/Code/ContentManagement/EntityFactory.cs
+++ CometClient/Assets/Code/ContentManagement/EntityFactory.cs
@@ -14,10 +14,10 @@ public class EntityFactory
 	{
 		GameObject entity = new GameObject("Entity#" + shapeDescription.entity_id);
 		entity.AddComponent<MeshFilter>();
-		entity.AddComponent<MeshRenderer>();
 		entity.GetComponent<MeshFilter>().mesh = MeshFactory.Create(shapeDescription.vertex_count, shapeDescription.triangle_count,
 			shapeDescription.vertices, shapeDescription.uvs, shapeDescription.triangles);
 		//TODO: Some kind of default material should be used here, so the fisheye could be turned off if desired.
+		entity.AddComponent<MeshRenderer>();
 		entity.GetComponent<MeshRenderer>().material = new Material(Resources.Load<Shader>("Fisheye"))
 		{
 			mainTexture = textureManager.GetTexture(shapeDescription.texture_id)
