7068c8d42f8237b36fe9d43c815ab46c8db5c2da
Máté Szabó <szmate1618@gmail.com>
2020-01-04 23:15:13 +0100

    add RegisterForm and ForgottenPasswordForm

--- MetaServer/project/users/forms.py
+++ MetaServer/project/users/forms.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import EmailField, StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
 
+
+class RegisterForm(FlaskForm):
+	email_address = EmailField('Email', validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+	submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
+	email_address = EmailField('Email', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Log In')
+
+class ForgottenPasswordForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Register')
