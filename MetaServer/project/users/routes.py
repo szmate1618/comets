899cd728477d13ae58bf71dc4b12d70303269e00
Máté Szabó <szmate1618@gmail.com>
2020-01-04 22:54:27 +0100

    add login and forgotten_password pages

--- MetaServer/project/users/routes.py
+++ MetaServer/project/users/routes.py
@@ -6,3 +6,11 @@ from . import users_blueprint
 @users_blueprint.route('/register')
 def register():
 	return render_template('users/register.html')
+
+@users_blueprint.route('/login')
+def login():
+	return render_template('users/login.html')
+
+@users_blueprint.route('/forgotten_password')
+def forgotten_password():
+	return render_template('users/forgotten_password.html')
