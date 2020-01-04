63dd22aa491e21fa4432505d4f9f0bd0f05104dd
Máté Szabó <szmate1618@gmail.com>
2020-01-04 21:42:24 +0100

    add base.html and index.html

--- MetaServer/metaserver.py
+++ MetaServer/metaserver.py
@@ -1,8 +1,11 @@
+from flask import render_template
+
 from project import create_app
 
 
 app = create_app('flask.cfg')
 
 @app.route('/')
+@app.route('/index')
 def index():
-	return "Hello World!"
+	return render_template('index.html')
