ed9e21efb6ff12749957cba48fb7c4e3353578e7
Máté Szabó <szmate1618@gmail.com>
2020-01-05 23:35:31 +0100

    add more comment, add TODO

--- MetaServer/project/__init__.py
+++ MetaServer/project/__init__.py
@@ -11,6 +11,8 @@ def create_app(config_filename=None):
 	db.init_app(app)
 
 	#This import must not precede the instantiation of `db`.
+	#That would cause 'cannot import' erros, because of the cyclic references.
+	#TODO: Eliminate cyclic references.
 	from project.users import users_blueprint
 	app.register_blueprint(users_blueprint)
 
