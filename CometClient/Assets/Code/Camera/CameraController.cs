799a0bfa98a3d71df517008232f63231fd55f9f1
Máté Szabó <szmate1618@gmail.com>
2022-02-19 19:29:27 +0100

    add camera zoom

---
+++ CometClient/Assets/Code/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float orthographicSize = Camera.main.orthographicSize;
+		orthographicSize += Input.GetAxis("zoom") * 10;
+		orthographicSize = Mathf.Clamp(orthographicSize, 5, 100);
+		Camera.main.orthographicSize = orthographicSize;
+	}
+}
