11faa4793e0ce9741c7ade2cdf6737294e90bd1f
Máté Szabó <szmate1618@gmail.com>
2022-02-19 23:36:28 +0100

    implement background infinite scrolling

--- CometClient/Assets/Code/ContentManagement/BackgroundManager.cs
+++ CometClient/Assets/Code/ContentManagement/BackgroundManager.cs
@@ -4,32 +4,65 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
+	public GameObject mainCamera;
 	public GameObject backgroundTile;
 	public int  columnCount;
 	public int  rowCount;
 	public float tileWidth;
 	public float tileHeight;
 
+	private GameObject[,] tiles;
+
 	void Start()
-    {
-		for (int i = 0; i < columnCount; i++)
+ {
+		tiles = new GameObject[rowCount, columnCount];
+		for (int i = 0; i < rowCount; i++)
 		{
-			for (int j = 0; j < rowCount; j++)
+			for (int j = 0; j < columnCount; j++)
 			{
-				GameObject newTile = Instantiate(backgroundTile);
-				newTile.transform.localScale = new Vector3(tileWidth, tileHeight, 1);
-				newTile.transform.position = new Vector3(
+				tiles[i, j] = Instantiate(backgroundTile);
+				tiles[i, j].transform.localScale = new Vector3(tileWidth, tileHeight, 1);
+				tiles[i, j].transform.position = new Vector3(
 					-(tileWidth * columnCount) / 2.0f + tileWidth / 2.0f + tileWidth * i, 
 					-(tileHeight * rowCount) / 2.0f + tileHeight / 2.0f + tileHeight * j,
 					9
 				);
 			}
 		}
-    }
+ }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ void Update()
+ {
+		for (int i = 0; i < rowCount; i++)
+		{
+			for (int j = 0; j < columnCount; j++)
+			{
+				float currentX = tiles[i, j].transform.position.x;
+				float currentY = tiles[i, j].transform.position.y;
+				float newX = currentX;
+				float newY = currentY;
+				if (currentX - mainCamera.transform.position.x > (tileWidth * columnCount) / 2.0f)
+				{
+					newX = currentX - tileWidth * columnCount;
+				}
+				if (currentX - mainCamera.transform.position.x < -(tileWidth * columnCount) / 2.0f)
+				{
+					newX = currentX + tileWidth * columnCount;
+				}
+				if (currentY - mainCamera.transform.position.y > (tileWidth * columnCount) / 2.0f)
+				{
+					newY = currentY - tileWidth * columnCount;
+				}
+				if (currentY - mainCamera.transform.position.y < -(tileWidth * columnCount) / 2.0f)
+				{
+					newY = currentY + tileWidth * columnCount;
+				}
+
+				if (newX != currentX || newY != currentY)
+				{
+					tiles[i, j].transform.position = new Vector3(newX, newY, tiles[i, j].transform.position.z);
+				}
+			}
+		}
+	}
 }
