ccb95e2ac71379bb1d9b43fd5719b74e76a52271
Máté Szabó <szmate1618@gmail.com>
2022-02-22 00:52:49 +0100

    use std::pair instead of std::tuple, use SingleLayerHashPartitioner as a collision partitioner

--- CometServer/Entities/SingleLayerHashPartitioner.cpp
+++ CometServer/Entities/SingleLayerHashPartitioner.cpp
@@ -21,11 +21,11 @@ namespace entity
 	void SingleLayerHashPartitioner::Insert(StaticEntity* entity)
 	{
 		//TODO: Insert into all containing partitions based on bounding box.
-		std::tuple<double, double> position{ entity->position.x, entity->position.y };
+		std::pair<double, double> position{ entity->position.x, entity->position.y };
 		if (partitions_map.count(position) == 0)
 		{
 			partitions.emplace_back();
-			partitions_map.insert(position, partitions[partitions.size() - 1]);
+			partitions_map[position] = &(partitions[partitions.size() - 1]);
 		}
 		partitions_map[position]->Add(entity);
 	}
